d5d63d47bef496ff105b95347dc8c125ff75efeb
tejbans <singh.tejbans@gmail.com>
2016-07-16 18:15:49 -0700

    Done.

--- js/app/app.js
+++ js/app/app.js
@@ -1,2 +1,4 @@
 angular
     .module('app', []);
+
+    
--- js/app/controllers/MainController.js
+++ js/app/controllers/MainController.js
@@ -1,7 +1,13 @@
 function MainController() {
-    this.name = 'Bill Gates';
+    this.name = 'Steve Jobs';
 }
+
+
 
 angular
     .module('app')
     .controller('MainController', MainController);
+
+function addNumbers(numberOne, numberTwo) {
+  return parseFloat(numberOne,10) + parseFloat(numberTwo,10);
+}
